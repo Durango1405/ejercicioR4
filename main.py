fb081814cdc038598b6b6cf9042b74c39b8132ae
Durango1405 <143657315+Durango1405@users.noreply.github.com>
2023-08-30 21:15:15 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# pedirle a juan su edad
+edad_juan = int(input("Ingrese la edad de juan: "))
+# calcular las edades de alberto, ana y la mamá
+edad_alberto = (2/3) * edad_juan
+edad_ana = (4/3) * edad_juan
+edad_mama = edad_juan + edad_alberto + edad_ana
+# resultado de las edades
+print("La edad de juan es:", edad_juan)
+print("La edad de alberto es:", edad_alberto)
+print("La edad de ana es:", edad_ana)
+print("La edad de la mamá es:", edad_mama)
